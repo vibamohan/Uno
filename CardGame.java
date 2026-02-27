cdb5a980b1f4670aee901bb2af9f32369ce32056
Viba Mohan <happymorning552@gmail.com>
2026-02-27 08:49:15 -0800

    non working networking
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>

--- CardGame.java
+++ CardGame.java
@@ -6,22 +6,54 @@ import java.util.Random;
 public class CardGame {
 
     private ArrayList<Card> deck, discardPile;
-    private ArrayList<Card> playerHand, aiHand;
+    private ArrayList<Card> player1Hand, player2Hand;
     private Card topCard;
-    private int currentPlayer; // 1 = human, 2 = AI
+    private int currentPlayer; // 1 = player 1, 2 = player 2
     private boolean gameOver;
+    private PlayerType player1Type;
+    private PlayerType player2Type;
+    private String player1Name = "Player 1";
+    private String player2Name = "Player 2";
+    private NetworkManager networkManager;
 
-    public CardGame() {
+    public CardGame(PlayerType p1Type, PlayerType p2Type) {
         deck = new ArrayList<>();
         discardPile = new ArrayList<>();
-        playerHand = new ArrayList<>();
-        aiHand = new ArrayList<>();
+        player1Hand = new ArrayList<>();
+        player2Hand = new ArrayList<>();
         currentPlayer = 1;
         gameOver = false;
+        player1Type = p1Type;
+        player2Type = p2Type;
         initDeck();
         shuffleDeck();
         dealCards();
         initTopCard();
+    }
+
+    public void setPlayerNames(String p1Name, String p2Name) {
+        this.player1Name = p1Name;
+        this.player2Name = p2Name;
+    }
+
+    public void setNetworkManager(NetworkManager nm) {
+        this.networkManager = nm;
+    }
+
+    public String getPlayer1Name() {
+        return player1Name;
+    }
+
+    public String getPlayer2Name() {
+        return player2Name;
+    }
+
+    public PlayerType getPlayer1Type() {
+        return player1Type;
+    }
+
+    public PlayerType getPlayer2Type() {
+        return player2Type;
     }
 
     private void initDeck() {
@@ -48,8 +80,8 @@ public class CardGame {
 
     private void dealCards() {
         for (int i = 0; i < 7; i++) {
-            playerHand.add(deck.remove(deck.size() - 1));
-            aiHand.add(deck.remove(deck.size() - 1));
+            player1Hand.add(deck.remove(deck.size() - 1));
+            player2Hand.add(deck.remove(deck.size() - 1));
         }
     }
 
@@ -63,11 +95,19 @@ public class CardGame {
     }
 
     public ArrayList<Card> getPlayerHand() {
-        return playerHand;
+        return player1Hand;
     }
 
     public ArrayList<Card> getAIHand() {
-        return aiHand;
+        return player2Hand;
+    }
+
+    public ArrayList<Card> getPlayer1Hand() {
+        return player1Hand;
+    }
+
+    public ArrayList<Card> getPlayer2Hand() {
+        return player2Hand;
     }
 
     public Card getTopCard() {
@@ -78,8 +118,8 @@ public class CardGame {
         return gameOver;
     }
 
-    public void playCard(Card card, String chosenColor, boolean isHuman) {
-        ArrayList<Card> hand = isHuman ? playerHand : aiHand;
+    public void playCard(Card card, String chosenColor, int playerNum) {
+        ArrayList<Card> hand = (playerNum == 1) ? player1Hand : player2Hand;
         if (!card.isPlayableOn(topCard)) {
             return;
         }
@@ -92,18 +132,16 @@ public class CardGame {
         // Handle special cards
         if (card.getType().equals("Skip") || card.getType().equals("Reverse")) {
             // 2-player: Skip/Reverse = skip opponent turn
-            if (isHuman) {
-                aiTurn();
-            }
+            switchTurn();
         } else if (card.getType().equals("Draw Two")) {
-            ArrayList<Card> target = isHuman ? aiHand : playerHand;
+            ArrayList<Card> target = (playerNum == 1) ? player2Hand : player1Hand;
             target.add(drawFromDeck());
             target.add(drawFromDeck());
         } else if (card.getType().equals("Wild")) {
             topCard = new Card(chosenColor, "Wild");
         } else if (card.getType().equals("Wild Draw Four")) {
             topCard = new Card(chosenColor, "Wild Draw Four");
-            ArrayList<Card> target = isHuman ? aiHand : playerHand;
+            ArrayList<Card> target = (playerNum == 1) ? player2Hand : player1Hand;
             for (int i = 0; i < 4; i++) {
                 target.add(drawFromDeck());
             }
@@ -112,15 +150,58 @@ public class CardGame {
         checkGameOver();
 
         if (!gameOver) {
-            if (isHuman) {
-                aiTurn();
-            }
+            switchTurn();
         }
     }
 
-    public void playerDrawCard() {
-        playerHand.add(drawFromDeck());
-        aiTurn();
+    private void switchTurn() {
+        currentPlayer = (currentPlayer == 1) ? 2 : 1;
+    }
+
+    public void applyRemoteMove(Card card, String chosenColor, int playerNum) {
+        // Apply a move received from the network WITHOUT switching turns
+        // (the turn was already switched by the sender)
+        ArrayList<Card> hand = (playerNum == 1) ? player1Hand : player2Hand;
+        if (!hand.contains(card)) {
+            return;
+        }
+
+        hand.remove(card);
+        discardPile.add(topCard);
+        topCard = card;
+
+        // Handle special cards
+        if (card.getType().equals("Skip") || card.getType().equals("Reverse")) {
+            // Turn already switched by sender, no additional action needed
+        } else if (card.getType().equals("Draw Two")) {
+            ArrayList<Card> target = (playerNum == 1) ? player2Hand : player1Hand;
+            target.add(drawFromDeck());
+            target.add(drawFromDeck());
+        } else if (card.getType().equals("Wild")) {
+            topCard = new Card(chosenColor, "Wild");
+        } else if (card.getType().equals("Wild Draw Four")) {
+            topCard = new Card(chosenColor, "Wild Draw Four");
+            ArrayList<Card> target = (playerNum == 1) ? player2Hand : player1Hand;
+            for (int i = 0; i < 4; i++) {
+                target.add(drawFromDeck());
+            }
+        }
+
+        checkGameOver();
+        // NOTE: Do NOT switch turn here - turn was already switched by the sender
+    }
+
+    public void playerDrawCard(int playerNum) {
+        ArrayList<Card> hand = (playerNum == 1) ? player1Hand : player2Hand;
+        hand.add(drawFromDeck());
+        switchTurn();
+    }
+
+    public void applyRemoteDraw(int playerNum) {
+        // Remote player drew a card. We add a card to their hand but don't switch turns
+        // (turn was already switched when they called playerDrawCard)
+        ArrayList<Card> hand = (playerNum == 1) ? player1Hand : player2Hand;
+        hand.add(drawFromDeck());
     }
 
     private Card drawFromDeck() {
@@ -143,20 +224,21 @@ public class CardGame {
     }
 
     private void checkGameOver() {
-        if (playerHand.isEmpty() || aiHand.isEmpty()) {
+        if (player1Hand.isEmpty() || player2Hand.isEmpty()) {
             gameOver = true;
         }
     }
 
-    private void aiTurn() {
+    public void performAITurn() {
         if (gameOver) {
             return;
         }
-        currentPlayer = 2;
 
         Card toPlay = null;
         String chosenColor = null;
-        for (Card c : aiHand) {
+        ArrayList<Card> hand = (currentPlayer == 1) ? player1Hand : player2Hand;
+
+        for (Card c : hand) {
             if (c.isPlayableOn(topCard)) {
                 toPlay = c;
                 break;
@@ -164,11 +246,12 @@ public class CardGame {
         }
 
         if (toPlay == null) {
-            aiHand.add(drawFromDeck());
+            hand.add(drawFromDeck());
+            switchTurn();
         } else {
             if (toPlay.getColor().equals("Wild")) {
                 int[] colorCount = new int[4];
-                for (Card c : aiHand) {
+                for (Card c : hand) {
                     switch (c.getColor()) {
                         case "Red":
                             colorCount[0]++;
@@ -195,29 +278,77 @@ public class CardGame {
             } else {
                 chosenColor = toPlay.getColor();
             }
-            playCard(toPlay, chosenColor, false);
+            playCard(toPlay, chosenColor, currentPlayer);
         }
-        currentPlayer = 1;
+    }
+
+    private void aiTurn() {
+        if (gameOver) {
+            return;
+        }
+
+        Card toPlay = null;
+        String chosenColor = null;
+        for (Card c : player2Hand) {
+            if (c.isPlayableOn(topCard)) {
+                toPlay = c;
+                break;
+            }
+        }
+
+        if (toPlay == null) {
+            player2Hand.add(drawFromDeck());
+        } else {
+            if (toPlay.getColor().equals("Wild")) {
+                int[] colorCount = new int[4];
+                for (Card c : player2Hand) {
+                    switch (c.getColor()) {
+                        case "Red":
+                            colorCount[0]++;
+                            break;
+                        case "Yellow":
+                            colorCount[1]++;
+                            break;
+                        case "Green":
+                            colorCount[2]++;
+                            break;
+                        case "Blue":
+                            colorCount[3]++;
+                            break;
+                    }
+                }
+                int maxIndex = 0;
+                for (int i = 1; i < 4; i++) {
+                    if (colorCount[i] > colorCount[maxIndex]) {
+                        maxIndex = i;
+                    }
+                }
+                String[] colors = {"Red", "Yellow", "Green", "Blue"};
+                chosenColor = colors[maxIndex];
+            } else {
+                chosenColor = toPlay.getColor();
+            }
+            playCard(toPlay, chosenColor, 2);
+        }
     }
 
     public int getCurrentPlayer() {
         return currentPlayer;
     }
 
-    public boolean canPressUNO() {
-        return playerHand.size() == 1;
+    public boolean canPressUNO(int playerNum) {
+        ArrayList<Card> hand = (playerNum == 1) ? player1Hand : player2Hand;
+        return hand.size() == 1;
     }
 
-    public void pressUNO(boolean humanPressed) {
-        if (humanPressed) {
-            if (playerHand.size() == 1) {
-                // Correct UNO
-                // Could show a message in GUI
-            } else {
-                // Penalize human: draw 2 cards
-                playerHand.add(drawFromDeck());
-                playerHand.add(drawFromDeck());
-            }
+    public void pressUNO(int playerNum) {
+        ArrayList<Card> hand = (playerNum == 1) ? player1Hand : player2Hand;
+        if (hand.size() == 1) {
+            // Correct UNO
+        } else {
+            // Penalize: draw 2 cards
+            hand.add(drawFromDeck());
+            hand.add(drawFromDeck());
         }
     }
 }
--- Table.java
+++ Table.java
@@ -2,6 +2,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.EOFException;
+import java.io.IOException;
 import java.util.ArrayList;
 import javax.sound.sampled.*;
 import java.io.File;
@@ -10,29 +12,114 @@ public class Table extends JPanel implements MouseListener {
 
     private CardGame game;
     private JButton drawButton, unoButton;
+    private GameMode gameMode;
+    private NetworkManager networkManager;
+    private boolean isPlayer1;
+    private Thread networkListenerThread;
 
-    public Table() {
+    public Table(CardGame game, GameMode mode, NetworkManager nm, boolean isPlayer1) {
         setLayout(null);
-        game = new CardGame();
+        this.game = game;
+        this.gameMode = mode;
+        this.networkManager = nm;
+        this.isPlayer1 = isPlayer1;
 
         drawButton = new JButton("Draw");
         drawButton.setBounds(600, 500, 100, 50);
         drawButton.addActionListener(e -> {
-            game.playerDrawCard();
-            playSound("draw");
-            repaint();
+            if (canCurrentPlayerMove()) {
+                game.playerDrawCard(isPlayer1 ? 1 : 2);
+                if (gameMode != GameMode.LOCAL_VS_AI && networkManager != null && networkManager.isConnected()) {
+                    try {
+                        networkManager.sendDrawCard();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                playSound("draw");
+                repaint();
+                startNetworkListener();
+            }
         });
         add(drawButton);
 
         unoButton = new JButton("UNO!");
         unoButton.setBounds(480, 500, 100, 50);
         unoButton.addActionListener(e -> {
-            game.pressUNO(true);
+            game.pressUNO(isPlayer1 ? 1 : 2);
             repaint();
         });
         add(unoButton);
 
         addMouseListener(this);
+
+        // Start network listener if in network mode
+        if (gameMode != GameMode.LOCAL_VS_AI && networkManager != null && networkManager.isConnected()) {
+            startNetworkListener();
+        }
+    }
+
+    private boolean canCurrentPlayerMove() {
+        int currentPlayer = game.getCurrentPlayer();
+        if (gameMode == GameMode.LOCAL_VS_AI) {
+            return currentPlayer == 1;
+        } else {
+            // Network mode - check if it's this player's turn
+            return (isPlayer1 && currentPlayer == 1) || (!isPlayer1 && currentPlayer == 2);
+        }
+    }
+
+    private void startNetworkListener() {
+        if (networkListenerThread != null && networkListenerThread.isAlive()) {
+            return;
+        }
+
+        networkListenerThread = new Thread(() -> {
+            try {
+                while (!game.isGameOver() && networkManager != null && networkManager.isConnected()) {
+                    try {
+                        NetworkManager.GameMove move = networkManager.receiveMove();
+                        if (move != null) {
+                            handleNetworkMove(move);
+                            SwingUtilities.invokeLater(() -> repaint());
+                        }
+                    } catch (EOFException e) {
+                        // Connection closed normally
+                        break;
+                    } catch (ClassNotFoundException e) {
+                        System.err.println("Network protocol error: " + e.getClass().getSimpleName());
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                // Connection lost or closed
+                System.out.println("Network disconnected.");
+            }
+        });
+        networkListenerThread.setDaemon(true);
+        networkListenerThread.start();
+    }
+
+    private void handleNetworkMove(NetworkManager.GameMove move) {
+        int otherPlayer = isPlayer1 ? 2 : 1;
+
+        if (move.cardColor.equals("DRAW")) {
+            // Remote opponent drew a card. Apply without switching turn (already switched by sender)
+            game.applyRemoteDraw(otherPlayer);
+        } else {
+            Card card = new Card(move.cardColor, move.cardType);
+            ArrayList<Card> otherHand = isPlayer1 ? game.getPlayer2Hand() : game.getPlayer1Hand();
+            if (otherHand.contains(card) || move.cardColor.equals("Wild")) {
+                // Find and remove the card
+                for (Card c : otherHand) {
+                    if (c.getColor().equals(move.cardColor) && c.getType().equals(move.cardType)) {
+                        // Use applyRemoteMove instead of playCard to avoid double turn-switch
+                        game.applyRemoteMove(c, move.chosenColor, otherPlayer);
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -43,10 +130,12 @@ public class Table extends JPanel implements MouseListener {
         if (game.isGameOver()) {
             g.setFont(new Font("Arial", Font.BOLD, 40));
             g.setColor(Color.WHITE);
-            if (game.getPlayerHand().isEmpty()) {
-                g.drawString("You Win!", 250, 250); 
-            }else {
-                g.drawString("AI Wins!", 250, 250);
+            ArrayList<Card> p1Hand = game.getPlayer1Hand();
+            ArrayList<Card> p2Hand = game.getPlayer2Hand();
+            if (p1Hand.isEmpty()) {
+                g.drawString(game.getPlayer1Name() + " Wins!", 150, 250);
+            } else {
+                g.drawString(game.getPlayer2Name() + " Wins!", 150, 250);
             }
             return;
         }
@@ -61,10 +150,10 @@ public class Table extends JPanel implements MouseListener {
         g.drawString(top.getType(), 365, 110);
         g.drawString(top.getColor(), 365, 130);
 
-        // Player hand
-        ArrayList<Card> hand = game.getPlayerHand();
-        for (int i = 0; i < hand.size(); i++) {
-            Card c = hand.get(i);
+        // Player hand (always at bottom, Player 1 if local, Player 1 if network server, Player 2 if network client)
+        ArrayList<Card> myHand = isPlayer1 ? game.getPlayer1Hand() : game.getPlayer2Hand();
+        for (int i = 0; i < myHand.size(); i++) {
+            Card c = myHand.get(i);
             g.setColor(c.getColorForGraphics());
             g.fillRect(50 + i * 70, 400, 60, 90);
             g.setColor(Color.BLACK);
@@ -72,30 +161,49 @@ public class Table extends JPanel implements MouseListener {
             g.drawString(c.getType(), 55 + i * 70, 450);
         }
 
-        // AI hand (show number of cards)
+        // Opponent hand (show number of cards)
+        ArrayList<Card> opponentHand = isPlayer1 ? game.getPlayer2Hand() : game.getPlayer1Hand();
         g.setColor(Color.WHITE);
         g.setFont(new Font("Arial", Font.BOLD, 18));
-        g.drawString("AI Cards: " + game.getAIHand().size(), 50, 50);
+        String opponentName = isPlayer1 ? game.getPlayer2Name() : game.getPlayer1Name();
+        g.drawString(opponentName + ": " + opponentHand.size() + " cards", 50, 50);
+
+        // Show current turn
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+        int currentPlayer = game.getCurrentPlayer();
+        String currentName = (currentPlayer == 1) ? game.getPlayer1Name() : game.getPlayer2Name();
+        g.drawString("Turn: " + currentName, 400, 250);
+
+        // Show if it's your turn
+        if (gameMode != GameMode.LOCAL_VS_AI && !canCurrentPlayerMove()) {
+            g.setColor(new Color(255, 200, 0));
+            g.setFont(new Font("Arial", Font.BOLD, 20));
+            g.drawString("Waiting for opponent...", 200, 150);
+        }
     }
 
     private void playSound(String type) {
         try {
             String file = type.equals("draw") ? "draw.wav" : "play.wav";
+            File soundFile = new File(file);
+            if (!soundFile.exists()) {
+                return; // Silently skip if sound file doesn't exist
+            }
             Clip clip = AudioSystem.getClip();
-            clip.open(AudioSystem.getAudioInputStream(new File(file)));
+            clip.open(AudioSystem.getAudioInputStream(soundFile));
             clip.start();
         } catch (Exception e) {
-            System.out.println("Sound error: " + e.getMessage());
+            // Silently ignore sound errors - they're not critical
         }
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if (game.isGameOver() || game.getCurrentPlayer() != 1) {
+        if (game.isGameOver() || !canCurrentPlayerMove()) {
             return;
         }
 
-        ArrayList<Card> hand = game.getPlayerHand();
+        ArrayList<Card> hand = isPlayer1 ? game.getPlayer1Hand() : game.getPlayer2Hand();
         for (int i = 0; i < hand.size(); i++) {
             int x = 50 + i * 70, y = 400, w = 60, h = 90;
             if (e.getX() >= x && e.getX() <= x + w && e.getY() >= y && e.getY() <= y + h) {
@@ -109,9 +217,36 @@ public class Table extends JPanel implements MouseListener {
                         return;
                     }
                 }
-                game.playCard(clicked, chosenColor, true);
+                game.playCard(clicked, chosenColor, isPlayer1 ? 1 : 2);
+
                 playSound("play");
                 repaint();
+
+                // Send move through network if applicable
+                if (gameMode != GameMode.LOCAL_VS_AI && networkManager != null && networkManager.isConnected()) {
+                    try {
+                        networkManager.sendMove(clicked, chosenColor);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    // Start listening for opponent's move
+                    startNetworkListener();
+                } else if (gameMode == GameMode.LOCAL_VS_AI && game.getPlayer2Type() == PlayerType.AI) {
+                    // Trigger AI turn for local game
+                    Thread aiThread = new Thread(() -> {
+                        try {
+                            Thread.sleep(500); // Small delay for better UX
+                            if (!game.isGameOver() && game.getCurrentPlayer() == 2) {
+                                game.performAITurn();
+                                SwingUtilities.invokeLater(() -> repaint());
+                            }
+                        } catch (InterruptedException ex) {
+                            Thread.currentThread().interrupt();
+                        }
+                    });
+                    aiThread.setDaemon(true);
+                    aiThread.start();
+                }
                 return;
             }
         }
